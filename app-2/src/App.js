e979dcb595047b7b2c00586aa6a0eddfb9ea378e
Truman Proctor <trumanproctor@Trumans-MacBook-Pro.local>
2018-12-01 11:05:05 -0700

    react drills 181201

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,19 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor() {
+    super()
+
+    this.state = {
+      foods: [
+        "Mexican",
+        "Italian",
+        "American",
+        "Thai",
+        "Indian"
+      ]
+    }
   }
-}
+  
+  render() {
+    let foodsOnScreen = this.state.foods.map( (element, index) =>{
+      return (
+        <h2 key={ index }>{ element }</h2>
+      )
+    })
+
+      return (
+        <div className="App">
+        { foodsOnScreen }
+        </div>
+      );
+    }
+  }
 
 export default App;
